a91c72da5c59ba630b9f006efacf7448cdc89049
azsu3598 <azsu@naver.com>
2023-11-11 13:48:44 +0900

    채팅 개선, 포스트페이지-작성자 컬러추가(그라데이션), 메인 페이지 추가중 -약간 부족한거 있어서 밤에추가

--- Chae/clients/src/App.js
+++ Chae/clients/src/App.js
@@ -1,72 +1,67 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import LandingPage from './components/views/LandingPage/LandingPage';
-import LoginPage from './components/views/LoginPage/LoginPage';
-import RegisterPage from './components/views/RegisterPage/RegisterPage';
-import Chat_Page_Login from './components/views/Chat_Page/Chat_Page_Login';
-import PostListPage from './components/views/PostPage/PostListPage';
-import PostPage from './components/views/PostPage/Post';
-import PostListPage1 from './components/views/PostPage/PostListPage1';
-import Post1 from './components/views/PostPage/Post1';
-import PostCreatePage from './components/views/PostPage/PostCreatePage'; // Import the PostCreatePage component
-import ChatPage from './components/views/ChatPage/ChatPage';
-import CalEpl from './components/views/callender/CalEpl';
-import CalLaliga from './components/views/callender/CalLaliga';
-import CalLigue1 from './components/views/callender/CalLigue1';
-import CalSeriea from './components/views/callender/CalSeriea';
-import CalBundesliga from './components/views/callender/CalBundesliga';
-import Team from './components/views/TeamPage/Team';
-import ChatRoomPage from './components/views/ChatPage/ChatRoomPage';
-import Chat from "./components/views/ChatPage/Chat"
-import UserList from './components/views/UserList/UserList';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import Chat_Page_Login from "./components/views/Chat_Page/Chat_Page_Login";
+import PostListPage from "./components/views/PostPage/PostListPage";
+import PostPage from "./components/views/PostPage/Post";
+import PostListPage1 from "./components/views/PostPage/PostListPage1";
+import Post1 from "./components/views/PostPage/Post1";
+import PostCreatePage from "./components/views/PostPage/PostCreatePage"; // Import the PostCreatePage component
+import ChatPage from "./components/views/ChatPage/ChatPage";
+import CalEpl from "./components/views/callender/CalEpl";
+import CalLaliga from "./components/views/callender/CalLaliga";
+import CalLigue1 from "./components/views/callender/CalLigue1";
+import CalSeriea from "./components/views/callender/CalSeriea";
+import CalBundesliga from "./components/views/callender/CalBundesliga";
+import Team from "./components/views/TeamPage/Team";
+import ChatRoomPage from "./components/views/ChatPage/ChatRoomPage";
+import Chat from "./components/views/ChatPage/Chat";
+import UserList from "./components/views/UserList/UserList";
 import Matching from "./components/views/Matching/matching";
-import EditPost from "./components/views/PostPage/EditPost"
-import TopScores from './components/views/TopPlayers/TopScores'
-import TopAssists from './components/views/TopPlayers/TopAssists';
+import EditPost from "./components/views/PostPage/EditPost";
+import TopScores from "./components/views/TopPlayers/TopScores";
+import TopAssists from "./components/views/TopPlayers/TopAssists";
 
 import Navbar from "./mainpage/Navbar";
-import TopPlayers from './components/views/TopPlayers/TopPlayers';
+import TopPlayers from "./components/views/TopPlayers/TopPlayers";
 import TeamRank from "./components/views/TeamRank/TeamRank";
 
-import Space1 from './components/views/HomPage/Space1';
-import Space2 from './components/views/HomPage/Space2';
-import Space3 from './components/views/HomPage/Space3';
-import Space4 from './components/views/HomPage/Space4';
-import './components/views/HomPage/Quadrants.css';
+import Space1 from "./components/views/HomPage/Space1";
+import Space2 from "./components/views/HomPage/Space2";
+import Space3 from "./components/views/HomPage/Space3";
+import Space4 from "./components/views/HomPage/Space4";
+import "./components/views/HomPage/Quadrants.css";
 
-import HotPost from './components/views/PostPage/HotPost'
+import HotPost from "./components/views/PostPage/HotPost";
 
-import Ch from './components/views/ChatPage/Ch';
+import Ch from "./components/views/ChatPage/Ch";
 
-import Footer from './components/views/Footer/Footer'
-
-import TeamStat from './components/views/TeamPage/TeamStat'
-import TeamView from './components/views/TeamPage/TeamView'
+import Footer from "./components/views/Footer/Footer";
+import MyPage from "./components/views/MyPage/MyPage";
+import TeamStat from "./components/views/TeamPage/TeamStat";
+import TeamView from "./components/views/TeamPage/TeamView";
 /* 홈페이지 레이아웃 사분면으로 구현 */
 function Quadrants() {
   return (
-    <div style={{ display: 'flex', height: '100vh' }}>
+    <div style={{ display: "flex", height: "100vh" }}>
       {/* 왼쪽 절반 (Space1) */}
       <div style={{ flex: 1 }}>
         <HotPost />
       </div>
 
       {/* 오른쪽 절반을 가로로 나눔 */}
-      <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+      <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
         {/* 위쪽 절반 (Space2) */}
-        <div style={{ flex: 1 }}>
-          <TopPlayers />
-        </div>
+        <div style={{ flex: 1 }}>{/* <TopPlayers /> */}</div>
 
         {/* 아래쪽 절반 (Space3) */}
-        <div style={{ flex: 1 }}>
-          <TeamRank />
-        </div>
+        <div style={{ flex: 1 }}>{/* <TeamRank /> */}</div>
       </div>
     </div>
   );
 }
-
 
 function App() {
   return (
@@ -79,22 +74,23 @@ function App() {
         <Route exact path="/register" element={<RegisterPage />} />
         {/* <Route exact path="/chat" element={<Chat_Page_Login />} /> */}
         <Route path="/post" element={<PostListPage1 />} />
-        <Route path="/post/create" element={<PostCreatePage />} /> // Add the route for PostCreatePage
+        <Route path="/post/create" element={<PostCreatePage />} /> // Add the
+        route for PostCreatePage
         <Route path="/post/:id" element={<Post1 />} />
-        <Route path='/chat' element={<Ch />} />
+        <Route path="/chat" element={<Ch />} />
         <Route exact path="/chat/:roomId" component={ChatRoomPage} />
-        <Route exact path='/cal/epl' element={<CalEpl />} />
-        <Route exact path='/cal/laliga' element={<CalLaliga />} />
-        <Route exact path='/cal/ligue1' element={<CalLigue1 />} />
-        <Route exact path='/cal/seriea' element={<CalSeriea />} />
-        <Route exact path='/cal/bundesliga' element={<CalBundesliga />} />
-        <Route exact path='/team/:teamName' element={<Team />} />
-        <Route exact path='/teamstat/:teamName' element={<TeamStat />} />
-        <Route exact path='/TeamView/:teamName' element={<TeamView />} />
-        <Route exact path='/userlist' element={<UserList />} />
+        <Route exact path="/cal/epl" element={<CalEpl />} />
+        <Route exact path="/cal/laliga" element={<CalLaliga />} />
+        <Route exact path="/cal/ligue1" element={<CalLigue1 />} />
+        <Route exact path="/cal/seriea" element={<CalSeriea />} />
+        <Route exact path="/cal/bundesliga" element={<CalBundesliga />} />
+        <Route exact path="/team/:teamName" element={<Team />} />
+        <Route exact path="/teamstat/:teamName" element={<TeamStat />} />
+        <Route exact path="/TeamView/:teamName" element={<TeamView />} />
+        <Route exact path="/userlist" element={<UserList />} />
         <Route exact path="/usermatch" element={<Matching />} />
         <Route path="/post/edit/:id" element={<EditPost />} />
-
+        <Route exact path="/mypage" element={<MyPage />} />
       </Routes>
       <Footer />
     </div>
--- Chae/server/models/User.js
+++ Chae/server/models/User.js
@@ -1,137 +1,175 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
-    name: {
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  phone: {
+    type: Number,
+    minlength: 10,
+    maxlength: 11,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+  logo: {
+    type: String,
+    default: `https://media-4.api-sports.io/football/leagues/39.png`,
+  },
+  matchingRequests: [
+    {
+      fromUser: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      date: {
+        type: Date,
+      },
+      time: {
+        hour: {
+          type: Number,
+        },
+        minute: {
+          type: Number,
+        },
+      },
+      location: {
         type: String,
-        maxlength: 50
+      },
+      accepted: {
+        type: Boolean,
+        default: false,
+      },
     },
-    email: {
+  ],
+  matchedEvents: [
+    {
+      fromUser: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      date: {
+        type: Date,
+      },
+      time: {
         type: String,
-        trim: true,
-        unique: 1
-    },
-    password: {
+      },
+      location: {
         type: String,
-        minlength: 5
+      },
     },
-    phone: {
-        type: Number,
-        minlength: 10,
-        maxlength: 11
+  ],
+  point: {
+    type: Number,
+    default: 0,
+  },
+  color: {
+    type: String,
+    default: "black",
+    get: function () {
+      const point = this.point;
+      if (point >= 0 && point <= 100) {
+        return "black";
+      } else if (point > 100 && point <= 200) {
+        return "red";
+      } else if (point > 200 && point <= 300) {
+        return "orange";
+      } else if (point > 300 && point <= 400) {
+        return "yellow";
+      } else if (point > 400 && point <= 500) {
+        return "green";
+      } else {
+        return "blue";
+      }
     },
-    role: {
-        type: Number,
-        default: 0
-    },
-    token: {
-        type: String
-    },
-    tokenExp: {
-        type: Number
-    },
-    matchingRequests: [{
-        fromUser: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        date: {
-            type: Date
-        },
-        time: {
-            hour: {
-                type: Number
-            },
-            minute: {
-                type: Number
-            }
-        },
-        location: {
-            type: String
-        },
-        accepted: {
-            type: Boolean,
-            default: false
-        }
-    }],
 
-    matchedEvents: [{
-        fromUser: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        date: {
-            type: Date
-        },
-        time: {
-            type: String
-        },
-        location: {
-            type: String
-        }
-    }]
-})
+    logo: {
+      type: String,
+      default: "https://media-4.api-sports.io/football/leagues/39.png",
+    },
+  },
+});
 
+userSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
 
-userSchema.pre('save', function (next) {
-    var user = this;
-    if (user.isModified('password')) {
-        //비밀번호를 암호화 시킨다.
-        bcrypt.genSalt(saltRounds, function (err, salt) {
-            if (err) return next(err)
-
-            bcrypt.hash(user.password, salt, function (err, hash) {
-                if (err) return next(err)
-                user.password = hash;
-                next();
-            })
-        })
-    } else {
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
         next();
-    }
-})
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 userSchema.methods.comparePassword = function (password, cb) {
-
-    //비밀번호 1234567  암호화된 비밀번호 
-    bcrypt.compare(password, this.password, function (err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch)
-    })
-}
+  //비밀번호 1234567  암호화된 비밀번호
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 userSchema.methods.generateToken = function (cb) {
-    var user = this;
-    //jsonwebtoken을 이용해서 token을 생성하기
-    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+  var user = this;
+  //jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
 
-    user.token = token;
-    user.save().then((user) => {
-        cb(null, user);
-    }).catch((err) => {
-        cb(err);
+  user.token = token;
+  user
+    .save()
+    .then((user) => {
+      cb(null, user);
     })
-}
+    .catch((err) => {
+      cb(err);
+    });
+};
 
 userSchema.statics.findByToken = function (token, cb) {
-    var user = this;
+  var user = this;
 
-    //토큰을 decode 한다.
-    jwt.verify(token, 'secretToken', function (err, decoded) {
-        //유저 아이디를 이용해서 유저를 찾은 다음에
-        // 클라이언트에서 가져온 token과 DB에 보관된 일치하는지 확인
+  //토큰을 decode 한다.
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    //유저 아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 token과 DB에 보관된 일치하는지 확인
 
-        user.findOne({ "_id": decoded, "token": token })
-            .then(user => {
-                cb(null, user);
-            }).catch((err) => {
-                cb(err);
-            })
-    })
-}
-const User = mongoose.model('User', userSchema);
+    user
+      .findOne({ _id: decoded, token: token })
+      .then((user) => {
+        cb(null, user);
+      })
+      .catch((err) => {
+        cb(err);
+      });
+  });
+};
+const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
